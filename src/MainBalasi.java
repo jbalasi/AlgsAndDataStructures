d0b57bbea4cea6ab7b5ed8a39349d61c1e2967ce
Jen Balasi <jbalasi@rochester.rr.com>
2018-03-04 17:16:02 -0500

    final

--- src/MainBalasi.java
+++ src/MainBalasi.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 
 //Jen Balasi
 //CSC406
@@ -6,7 +7,7 @@ import java.io.File;
 
 public class MainBalasi {
 	
-	public static void main (String [] args) {
+	public static void main (String [] args) throws IOException {
 		if(args.length == 0) {
 			System.out.println("The file entered was not valid.");
 			System.exit(1);
--- src/SudokuToSatReducerBalasi.java
+++ src/SudokuToSatReducerBalasi.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
@@ -10,16 +12,10 @@ import java.util.regex.Pattern;
 
 
 public class SudokuToSatReducerBalasi {
-	File file;
+	private BufferedWriter writer;
 	TimerBalasi timer = new TimerBalasi();
 	int width, height;
-	String  tempFileName  = "temp.cnf";
 	SudokuBoardBalasi board;
-//	try {
-//		Writer  writer=new PrintWriter("SATFile.txt");
-//	}catch(IOException e) {
-//		
-//	}
 
 	int compare, row, column, value;
 	int boardSize = 9;
@@ -28,20 +24,9 @@ public class SudokuToSatReducerBalasi {
 		createBoard(file);
 	}
 	
-	public void output() {
-		
-	}
-
 	public void createBoard(File file) {
 		try {
 			Scanner scan = new Scanner(file);
-			Pattern p = Pattern.compile("p cnf");
-			Matcher m =p.matcher("p cnf");
-			boolean b = m.matches();
-			
-			if(b == true) {
-				
-			while(scan.findInLine(p)==null) { 
 			
 				this.width = Integer.parseInt(scan.next());
 				this.height = Integer.parseInt(scan.next());
@@ -53,9 +38,7 @@ public class SudokuToSatReducerBalasi {
 					board.boardCells[i]=num;
 					
 				}
-				break;
-				}	
-			}
+			
 		}catch (Exception e) {
 				System.out.println("There was an error "+e);
 			}		
@@ -63,115 +46,115 @@ public class SudokuToSatReducerBalasi {
 }
 	
 
-	public void reduceBoard() {
+	public void reduceBoard() throws IOException {
+		File output=new File("output.txt");
+		writer = new BufferedWriter (new FileWriter(output));
+		writer.write("p 729 11988");
 		timer.start();//call timer start 
 		atleastOneInRow();
 		timer.stop();//call timer stop
 		System.out.println("This ran for "+timer.getDuration()+"ms.");
+		writer.close();
 	}
 	
 	//i = row, j= column, k=value
-	public void atleastOneInRow() {
+	public void atleastOneInRow() throws IOException {
+		writer.write(System.getProperty( "line.separator" ));
 		int [] row = new int[9];
 		int[] box = new int[9];
 		for(int k=1; k<=boardSize; k++) {
 			for(int i=0; i<boardSize; i++) {
 				for(int j=0; j<boardSize; j++) {
-					System.out.print(String.valueOf(encode(i,j,k)) + " ");
-					row[i]=(encode (board.row(box[i]),board.column(box[i]),k));
+					row[i]=(encode(i,j,k));
 				}
-				System.out.print(" 0");
-				System.out.println("");
 			}
-		}atmostOneInRow(row);
+		}
+		atmostOneInRow(row);
 	}
 	
 	
-	public void atmostOneInRow(int[] row) {	
+	public void atmostOneInRow(int[] row) throws IOException {	
 		row= new int[9];
 		int[] box = new int[9];
 			for(int i=0; i<boardSize; i++) {
 				for(int j=0; j<boardSize; j++) {
 					for(int k=1; k>9; k++) {
-						System.out.print(String.valueOf(encode(i,j,k))+" ");
-						row[i]=(encode (board.row(box[i]),board.column(box[i]),k));
+						row[i]=(encode(i,j,k));
 				}
-				System.out.print(" 0");
-				System.out.println("");	
 			}
 		}atLeastOneInCol(row);
 	}
 	
-	public void atLeastOneInCol(int[] row) {
+	public void atLeastOneInCol(int[] row) throws IOException {
 		row= new int[9];
 		int[] box = new int[9];
 		for(int k=1; k<boardSize; k++) {
 			for(int j=0; j<boardSize; j++) {
 				for(int i=0; i<boardSize; i++) {
-					System.out.print(String.valueOf(encode(i,j,k))+" ");
-					row[i]=(encode (board.row(box[i]),board.column(box[i]),k));
+					row[i]=(encode(i,j,k));
 				}
-				System.out.print(" 0");
-				System.out.println("");
 			}
 		}atMostOneInCol(row);
 	}
 	
-	public void atMostOneInCol(int[] row) {				//check this
+	public void atMostOneInCol(int[] row) throws IOException {				//check this
 		int[] arr= new int[9];
 		int[] box = new int[9];
 		for(int j=0; j<boardSize; j++) {
 			for(int i=0; i<boardSize; i++) {
 				for(int k=9; k>0; k--) {
-					System.out.print(String.valueOf(encode(i,j,k))+" ");
-					row[i]=(encode (board.row(box[i]),board.column(box[i]),k));
+					row[i]=(encode(i,j,k));
 			}
-			System.out.print(" 0");
-			System.out.println("");	
 		}
 	}atleastOneInBox(row);
 	}
 	
-	public void atleastOneInBox(int[] row) {
+	public void atleastOneInBox(int[] row) throws IOException {
 		row =new int[9];
 		int[] box = new int[9];
 		for(int k=1; k<=9; k++) {
 			for(int j=0; j<9; j++) {
 				box= board.getBoxValues(j);
 				for(int i=0; i<9; i++) {
-					System.out.print(encode (board.row(box[i]),board.column(box[i]),k) + " ");
-					row[i]=(encode (board.row(box[i]),board.column(box[i]),k));
+					row[i]=(encode(i,j,k));
 				}
-				System.out.print(" 0");
-				atmostOneInBox(row);
-				System.out.println("");
 			}
 		}atmostOneInBox(row);
 	}
 	
-	public void atmostOneInBox(int[] arr) {
+	public void atmostOneInBox(int[] arr) throws IOException {
 		int k=0;
 		int []row =new int[9];
 		int[] box = new int[9];
 		for(int i=0; i<arr.length; i++) {
 			for(int j=i+1; j<arr.length; j++) {
-				System.out.println(-arr[i]+" "+-arr[j]);
-				row[i]=(encode (board.row(box[i]),board.column(box[i]),k));
+				row[i]=(encode(i,j,k));
+				writer.write(-row[i]+" "+-row[j]);
+				writer.write(System.getProperty( "line.separator" ));
 			}
+
 		}
 	}	
-	
+	 public void atLeastOneValue(int size) {
+		 int[] row = new int[0];
+			for(int i=0; i<size; i++) {
+				for(int j=0; j<size; j++) {
+					for(int k=1; k<=size; k++) {
+						row[k-1]=encode(i,j,k);
+								writer.write(row[k-1]+" ");
+					}
+				}
+			}atMostOneValue(row);
+	 }
+	 
+	 public void atMostOneValue(int [] arr){
+			for (int i=0; i<arr.length; i++){
+				
+			}
+		}
 	private int encode(int i, int j, int k) {
 		return (i*boardSize*boardSize) +(j*boardSize)+k;
 	}
-	
-//	  void write (String str) {
-//
-//          try {
-//
-//               writer.write(str);
-//               writer.flush();
-//         }  catch(IOException  e)  { }
 
 	
 }
